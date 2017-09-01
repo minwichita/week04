0ea079f4fa5854f71b2bfdfe1bc5b8f70f7c5c33
minwichita <59030206@kmitl.ac.th>
2017-09-01 10:44:16 +0700

    Edited-w4p03

--- lab01/lab01/Program.cs
+++ lab01/lab01/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace lab01
 {
@@ -10,6 +6,10 @@ namespace lab01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
+            Console.WriteLine(" World.");
+
+
         }
     }
 }
