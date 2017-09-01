1a5e2dd0784d3e1c260f5af750d4157c0671d6c6
minwichita <59030206@kmitl.ac.th>
2017-09-01 11:31:24 +0700

    Edited-w4p08

--- lab01/lab01/Program.cs
+++ lab01/lab01/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace lab01
 {
@@ -10,6 +7,13 @@ namespace lab01
     {
         static void Main(string[] args)
         {
+            const double i = 123.456789d;
+            Console.WriteLine("{0:F1}", i);
+            Console.WriteLine("{0:F2}", i);
+            Console.WriteLine("{0:F3}", i);
+            Console.WriteLine("{0:F4}", i);
+            Console.WriteLine("{0:F5}", i);
+
         }
     }
 }
