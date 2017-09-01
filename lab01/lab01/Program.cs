771cebcbee5f3887256c7a0713e27f318ef797a2
minwichita <59030206@kmitl.ac.th>
2017-09-01 12:06:28 +0700

    Edited -w4p09

--- lab01/lab01/Program.cs
+++ lab01/lab01/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace lab01
 {
@@ -10,6 +7,11 @@ namespace lab01
     {
         static void Main(string[] args)
         {
+            char ch;
+            Console.Write("Press a key followed by ENTER:");
+            ch = (char)Console.Read();  // get a char
+            Console.WriteLine("Your key is: " + ch);
+
         }
     }
 }
