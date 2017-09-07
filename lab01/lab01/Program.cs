09e6ca535642c9764d602eb04a00974b7e6a9582
minwichita <59030206@kmitl.ac.th>
2017-09-07 16:47:59 +0700

    Edited-w4p18

--- lab01/lab01/Program.cs
+++ lab01/lab01/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace lab01
 {
@@ -10,6 +7,16 @@ namespace lab01
     {
         static void Main(string[] args)
         {
+            const double lightSpeed = 186000d;   // miles per second
+            Console.WriteLine("Light speed = {0} Mile Per second", lightSpeed);
+            const double mileTokm = 1.609344;
+            Console.WriteLine("Light speed = {0} km Per second", lightSpeed * mileTokm);
+            const double SunToEarthDistance = 93000000d;  // miles
+            Console.WriteLine("SunToEarthDistance = {0} km", SunToEarthDistance * mileTokm);
+            double SunToEarthTimeOfLight = SunToEarthDistance / lightSpeed;  // miles
+            Console.WriteLine("SunToEarthTimeOfLight = {0} seconds", SunToEarthTimeOfLight);
+            Console.WriteLine("SunToEarthTimeOfLight = {0} minutes", SunToEarthTimeOfLight / 60d);
+
         }
     }
 }
