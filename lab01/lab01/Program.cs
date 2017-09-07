754cb24248fd8b9f48a4ebc792259c351a3d9e3f
minwichita <59030206@kmitl.ac.th>
2017-09-07 12:30:12 +0700

    Edited-w4p10

--- lab01/lab01/Program.cs
+++ lab01/lab01/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace lab01
 {
@@ -10,6 +7,12 @@ namespace lab01
     {
         static void Main(string[] args)
         {
+            string str;
+            Console.Write("Enter some characters.");
+            str = Console.Read();
+            Console.WriteLine("You entered: " + str);
+
         }
     }
 }
+
