536f8bc527a67ee90d78103f9212743da1b4b0d0
minwichita <59030206@kmitl.ac.th>
2017-09-07 16:24:44 +0700

    Edited-w4p14

--- lab01/lab01/Program.cs
+++ lab01/lab01/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace lab01
 {
@@ -10,6 +7,10 @@ namespace lab01
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+            Console.WriteLine(randomNumber);
+
         }
     }
 }
