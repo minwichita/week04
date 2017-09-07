3f88ceffc8e8eb2ac864bda8e3e7352ea3dddf3f
minwichita <59030206@kmitl.ac.th>
2017-09-07 15:54:23 +0700

    Edited-w4p13

--- lab01/lab01/Program.cs
+++ lab01/lab01/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace lab01
 {
@@ -10,6 +7,10 @@ namespace lab01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Data type : int");
+            Console.WriteLine("Size :" + sizeof(int));
+            Console.WriteLine("Minimum Value :" + int.MinValue);
+            Console.WriteLine("Maximum Value :" + int.MaxValue);
         }
     }
 }
