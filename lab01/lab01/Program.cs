5670952ad0f1576222151970e11d44387412616c
minwichita <59030206@kmitl.ac.th>
2017-09-07 18:31:55 +0700

    Edited-w4p06

--- lab01/lab01/Program.cs
+++ lab01/lab01/Program.cs
@@ -8,12 +8,12 @@ namespace lab01
         static void Main(string[] args)
         {
             int n = 123456789;
-            Console.WriteLine("{0, E}", 1);
-            Console.WriteLine("{0, F}", 1);
-            Console.WriteLine("{0, G}", 1);
-            Console.WriteLine("{0, N}", 1);
-            Console.WriteLine("{0, P}", 1);
-            Console.WriteLine("{0, X}", 1);
+            Console.WriteLine("{0, 0:E}", n);
+            Console.WriteLine("{0, 0:F}", n);
+            Console.WriteLine("{0, 0:G}", n);
+            Console.WriteLine("{0, 0:N}", n);
+            Console.WriteLine("{0, 0:P}", n);
+            Console.WriteLine("{0, 0:X}", n);
 
         }
     }
